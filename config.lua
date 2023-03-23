1fc3c38bfe3092013ef20c086a9f899b4597e0b5
Jos Dehaes <jos.dehaes@gmail.com>
2023-03-23 09:16:13 +0100

    vscode theme for lualine

--- config.lua
+++ config.lua
@@ -45,6 +45,14 @@ if ok then
 	})
 end
 
+require("lualine").setup({
+	options = {
+		-- ...
+		theme = "vscode",
+		-- ...
+	},
+})
+
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
 	"lua",
