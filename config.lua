b54d4b655c31d5b23accdd96d870a94b666d92e0
Jos Dehaes <jos.dehaes@gmail.com>
2025-02-05 23:21:58 +0000

    simplify

--- config.lua
+++ config.lua
@@ -1,8 +1,7 @@
-local _, _ = pcall(require, "impatient")
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-lvim.colorscheme = "lunar"
+lvim.colorscheme = "vscode"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -10,15 +9,12 @@ lvim.colorscheme = "lunar"
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
 lvim.leader = "space"
 
+
 vim.cmd("set showmatch")
 vim.cmd("set wildmode=list,longest")
 
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-vim.cmd("nmap <F3> :lua require('dap').toggle_breakpoint()<CR>")
-vim.cmd("nmap <F5> :lua require('dap').step_into()<CR>")
-vim.cmd("nmap <F6> :lua require('dap').step_over()<CR>")
-vim.cmd("nmap <F7> :lua require('dap').step_out()<CR>")
-vim.cmd("nmap <F9> :SymbolsOutline<CR>")
+lvim.keys.insert_mode["<C-s>"] = ":w<cr>"
 lvim.builtin.terminal.open_mapping = "<c-t>"
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
@@ -28,38 +24,11 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.treesitter.rainbow.enable = true
 
-local ok, ts = pcall(require, "nvim-treesitter-configs")
-if ok then
-	ts.setup({
-		rainbow = {
-			enable = true,
-			-- list of languages you want to disable the plugin for
-			disable = { "jsx", "cpp" },
-			-- Which query to use for finding delimiters
-			query = "rainbow-parens",
-			-- Highlight the entire buffer all at once
-			strategy = require("ts-rainbow.strategy.global"),
-			-- Do not enable for files with more than n lines
-			max_file_lines = 3000,
-		},
-	})
-end
-
--- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
 	"lua",
 	"rust",
 	"toml",
 }
-
-local ok, so = pcall(require, "symbols-outline")
-if ok then
-	so.setup()
-end
-local ok, hl = pcall(require, "hlargs")
-if ok then
-	hl.setup()
-end
 
 vim.g.copilot_filetypes = {
 	["*"] = false,
@@ -79,208 +48,40 @@ lvim.builtin.which_key.mappings["t"] = {
 	r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
 }
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
 
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
 	{ command = "stylua", filetypes = { "lua" } },
-	{ command = "rustfmt", filetypes = { "rust" } },
 })
 
-local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
-local codelldb_adapter = {
-	type = "server",
-	port = "${port}",
-	executable = {
-		command = mason_path .. "bin/codelldb",
-		args = { "--port", "${port}" },
-		-- On windows you may have to uncomment this:
-		-- detached = false,
-	},
-}
-
-pcall(function()
-	require("rust-tools").setup({
-		tools = {
-			executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
-			reload_workspace_from_cargo_toml = true,
-			runnables = {
-				use_telescope = true,
-			},
-			inlay_hints = {
-				auto = true,
-				only_current_line = false,
-				show_parameter_hints = false,
-				parameter_hints_prefix = "<-",
-				other_hints_prefix = "=>",
-				max_len_align = false,
-				max_len_align_padding = 1,
-				right_align = false,
-				right_align_padding = 7,
-				highlight = "Comment",
-			},
-			hover_actions = {
-				border = "rounded",
-			},
-			on_initialized = function()
-				vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-					pattern = { "*.rs" },
-					callback = function()
-						local _, _ = pcall(vim.lsp.codelens.refresh)
-					end,
-				})
-			end,
-		},
-		dap = {
-			adapter = codelldb_adapter,
-		},
-		server = {
-			on_attach = function(client, bufnr)
-				require("lvim.lsp").common_on_attach(client, bufnr)
-				require("illuminate").on_attach(client)
-				local rt = require("rust-tools")
-				vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-			end,
-			capabilities = require("lvim.lsp").common_capabilities(),
-			settings = {
-				["rust-analyzer"] = {
-					assist = {
-						importEnforceGranularity = true,
-						importPrefix = "crate",
-					},
-					lens = {
-						enable = true,
-					},
-					checkOnSave = {
-						enable = true,
-						command = "check",
-					},
-					inlayHints = {
-						lifetimeElisionHints = {
-							enable = true,
-							useParameterNames = true,
-						},
-					},
-				},
-			},
-		},
-	})
-end)
-
-lvim.builtin.dap.on_config_done = function(dap)
-	dap.adapters.codelldb = codelldb_adapter
-	dap.configurations.rust = {
-		{
-			name = "Launch file",
-			type = "codelldb",
-			request = "launch",
-			program = function()
-				return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-			end,
-			cwd = "${workspaceFolder}",
-			stopOnEntry = false,
-		},
-	}
-end
-
-vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
 
 lvim.builtin.which_key.mappings["r"] = {
 	name = "Rust",
-	r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+	r = { "<cmd>RustLsp runnables<Cr>", "Runnables" },
 	t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
-	m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
-	c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
-	p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
-	d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
-	v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
-	R = {
-		"<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
-		"Reload Workspace",
-	},
-	o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
-	y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
-	P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
-	i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
-	f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
-	D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
+	m = { "<cmd>RustLsp expandMacro<Cr>", "Expand Macro" },
+	c = { "<cmd>RustLsp openCargo<Cr>", "Open Cargo" },
+	p = { "<cmd>RustLsp parentModule<Cr>", "Parent Module" },
+	d = { "<cmd>RustLsp debuggables<Cr>", "Debuggables" },
+	v = { "<cmd>RustLsp crateGraph<Cr>", "View Crate Graph" },
+	R = { "<cmd>RustLsp reloadWorkspace", "Reload Workspace", },
+	o = { "<cmd>RustLsp externalDocs<Cr>", "Open External Docs" },
 }
 
 lvim.plugins = {
-	"simrat39/rust-tools.nvim",
 	"Mofiqul/vscode.nvim",
-	"HiPhish/nvim-ts-rainbow2",
-	"preservim/tagbar",
-	"lewis6991/impatient.nvim",
-	"m-demare/hlargs.nvim",
 	"github/copilot.vim",
-	{
-		"saecki/crates.nvim",
-		version = "v0.3.0",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("crates").setup({
-				null_ls = {
-					enabled = true,
-					name = "crates.nvim",
-				},
-				popup = {
-					border = "rounded",
-				},
-			})
-		end,
-	},
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
 	{
 		"j-hui/fidget.nvim",
-		config = function()
-			require("fidget").setup()
-		end,
-	},
-	{
-		"simrat39/symbols-outline.nvim",
-		config = function()
-			require("symbols-outline").setup()
-		end,
 	},
 	{
 		"folke/trouble.nvim",
 		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("trouble").setup()
-		end,
-	},
-	{
-		"ldelossa/litee.nvim",
-		config = function()
-			require("litee.lib").setup({})
-		end,
-	},
-	{
-		"ldelossa/litee-calltree.nvim",
-		config = function()
-			require("litee.calltree").setup({})
-		end,
-	},
-	{
-		"romgrk/nvim-treesitter-context",
-		config = function()
-			require("treesitter-context").setup({
-				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-				throttle = true, -- Throttles plugin updates (may improve performance)
-				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-				patterns = {
-					-- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-					-- For all filetypes
-					-- Note that setting an entry here replaces all other patterns for this entry.
-					-- By setting the 'default' entry below, you can control which nodes you want to
-					-- appear in the context window.
-					default = {
-						"class",
-						"function",
-						"method",
-					},
-				},
-			})
-		end,
 	},
 }
